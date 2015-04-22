872c8266c5ff46ae6f1fca140596660f6c53c49b
ajmckee <aj.mckee@druid-dns.com>
2015-04-22 15:25:44 +0000

    system compile

--- definitions/services.js
+++ definitions/services.js
@@ -9,9 +9,9 @@ exports.elasticsearch = {
   specific: {
     name: 'dockerfile/elasticsearch',
     execute: {
-      args: '-d -p 9200:9200 -p 9300:9300 -v /mnt/elasticsearch:/data',
+		args: '-d -p 9200:9200 -p 9300:9300 -p 54328:54328 -v /mnt/elasticsearch:/data',
       //exec: '/elasticsearch/bin/elasticsearch --network.bind_host=0.0.0.0 --network.publish_host=172.17.42.1'
-      exec: '/elasticsearch/bin/elasticsearch --network.bind_host=localhost --network.publish_host=__TARGETIP__'
+      	exec: '/elasticsearch/bin/elasticsearch --network.bind_host=0.0.0.0 --network.publish_host=__TARGETIP__ --discovery.zen.ping.unicast.hosts=172.31.30.52,172.31.30.51'
     }
   }
 };
