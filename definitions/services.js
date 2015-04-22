8b57bd651238397a9dc22b5d980b7485eaa628b6
AJ McKee <aj.mckee@ramjam.io>
2015-04-22 13:10:53 +0100

    first commit

---
+++ definitions/services.js
@@ -0,0 +1,8 @@
+// Place service container definitions here.
+exports.root = {
+	type: 'blank-container'
+};
+
+exports.elasticsearch = {
+	type: 'docker'
+};
---
+++ system.js
@@ -0,0 +1,15 @@
+// Define the system topology here. The topolgy should reference 
+// containers defined in definitions/services.js
+
+exports.name = 'esaj';
+exports.namespace = 'esaj';
+exports.id = '2541616a-5d56-4690-aecd-bb6fd3224b6f';
+
+exports.topology = {
+	local: {
+		root: ['elasticsearch']
+	},
+	development: {
+		root: ['elasticsearch']
+	}
+};
