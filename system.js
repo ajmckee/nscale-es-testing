eefa7614bdfdbe0e456285ced30124d123505bc2
AJ McKee <aj.mckee@ramjam.io>
2015-04-22 13:53:50 +0100

    Really

--- system.js
+++ system.js
@@ -5,10 +5,6 @@ exports.name = 'esaj';
 exports.namespace = 'esaj';
 exports.id = '2541616a-5d56-4690-aecd-bb6fd3224b6f';
 
-exports.root = {
-	type: 'blank-container'
-};
-
 exports.topology = {
 	local: {
 		root: ['elasticsearch']
